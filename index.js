b45d0cfb2499c4cb0e9aca8e835b889696a1b285
Gant <GantMan@gmail.com>
2017-11-16 09:15:18 -0600

    testing cool thangs

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ module.exports = (robot) => {
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
+
+  // https://developer.github.com/webhooks/
+
+  // Let's see what happens every time a project is created
+  robot.on('project.created', async context => {
+    // A new issue was opened, what should we do with it?
+    robot.log(context)
+  })
+
+
 }
